b7f67ec27966e7ed56a9db07a4b309e44690b7b1
Imanol-Acosta <ilacosta05@hotmail.com>
2025-07-14 21:15:31 -0400

    arreglando contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -2,11 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace Imanol_Acosta_AP1_P2.DAL;
+
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     public DbSet<Producto> Productos { get; set; }
+    public DbSet<Entrada> Entradas { get; set; }
+
+    public DbSet<EntradaDetalle> EntradaDetalles { get; set; }
+
 }
-
-
-
