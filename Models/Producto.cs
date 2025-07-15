2002787b741681659e48375c6ea01f55686bb673
Imanol-Acosta <ilacosta05@hotmail.com>
2025-07-14 20:04:52 -0400

    Agregando models, y arreglando nombre

---
+++ Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Imanol_Acosta_AP1_P2.Models;
+
+public class Producto
+{
+    [Key]
+
+    public int ProductoID { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public double Peso { get; set; }
+    public double Existencia { get; set; }
+    public bool EsCompuesto { get; set; }
+
+}
+
