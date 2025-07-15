bc7e849c9b7be0b173baa7a928ea8cc6acd9fb3a
Imanol-Acosta <ilacosta05@hotmail.com>
2025-07-14 20:04:37 -0400

    Agregando codigo contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,7 +5,7 @@ namespace Imanol_Acosta_AP1_P2.DAL;
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-    public DbSet<Detalle> Detalles { get; set; }
+    public DbSet<Producto> Productos { get; set; }
 }
 
 
